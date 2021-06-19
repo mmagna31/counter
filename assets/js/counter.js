226e60bc3b58c95343ee0641c4ba2691b22c0b33
mmagna31 <marco.magnarelli@gmail.com>
2021-06-20 01:20:22 +0200

    set message counter

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -37,6 +37,19 @@ function createParagraph(text) {
   return para;
 }
 
+
+function defineMessage(num, min, max) {
+  let message;
+  if (num < min) {
+    message = `Counter can't be less than ${COUNTER_MIN}. Please increase counter.`;
+  } else if (num > max) {
+    message = `Counter can't be greater than ${COUNTER_MAX}. Please decrease counter.`;
+  } else {
+    message = `Counter ${counterHTML} is not a valid number`;
+  }
+  return message;
+}
+
 /* Define layout */
 
 const COUNTER_MIN = 0;
@@ -45,9 +58,11 @@ const COUNTER_MAX = 10;
 let counterPara = createParagraph(COUNTER_MIN);
 let increaseBtn = createBtn("+", "increase", {height: "30px", width: "30px"});
 let decreaseBtn = createBtn("-", "decrease", {height: "30px", width: "30px"});
+let counterMessage = createParagraph("");
+counterMessage.hidden = true;
 
 /* took from html */
-counterDiv.append(counterPara, decreaseBtn, increaseBtn);
+counterDiv.append(counterPara, decreaseBtn, increaseBtn, counterMessage);
 counterDiv.addEventListener("click", (event) => {
 
   let action = event.target.dataset.action;
@@ -63,10 +78,13 @@ counterDiv.addEventListener("click", (event) => {
   }
 
   if (isValidCounter(counterHTML, COUNTER_MIN, COUNTER_MAX)) {
+    if (!counterMessage.hidden) {
+      counterMessage.hidden = true;
+    }
     setCounterHTML(counterPara, counterHTML);
   } else {
-    /* stampa messaggio */
-     return false;
+    counterMessage.innerHTML = defineMessage(counterHTML, COUNTER_MIN, COUNTER_MAX);
+    counterMessage.hidden = false;
   }
 
 });
