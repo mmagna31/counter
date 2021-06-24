f0a8b0f2a97e8bbfc841fc454bb712ddb0148a94
mmagna31 <marco.magnarelli@gmail.com>
2021-06-24 17:56:32 +0200

    improving code readability

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,94 +1,40 @@
-function increaseCounter(num) {
-  console.log("increaseCounter", num);
-  ++num;
-  console.log("increaseCounter", num);
-  return num;
-}
+/*
+  Counter Layout: Initialize the counter and create buttons to play with the counter.
+  This layout uses the counterDiv element set in the html file. 
+*/
 
-function decreaseCounter(num) {
-  return --num;
-}
-
-function isValidCounter(num, min, max) {
-  return num >= min && num <= max;
-}
-
-function getCounterHTML(elem) {
-  return elem.innerHTML;
-}
-
-function setCounterHTML(elem, num) {
-  elem.innerHTML = num;
-}
-
-function createBtn(text, action) {
-  let btn = document.createElement("button");
-  btn.innerHTML = text;
-  btn.setAttribute("data-action", action);
-  return btn;
-}
-
-// function createParagraph(text = "") {
-//   let para = document.createElement("p");
-//   para.innerHTML = text;
-//   return para;
-// }
-function createParagraph(text = "", styleObj = {}) {
-  let para = document.createElement("p");
-  para.innerHTML = text;
-  /* define style for para */
-  Object.entries(styleObj).map(([key, value]) => para.style[key] = value);
-
-  return para;
-}
-
-function defineMessage(num, min, max) {
-  let message;
-  if (num < min) {
-    message = `Counter can't be less than ${min}. Please increase counter.`;
-  } else if (num > max) {
-    message = `Counter can't be greater than ${max}. Please decrease counter.`;
-  } else {
-    message = `Counter ${num} is not a valid number`;
-  }
-  return message;
-}
-
-function defineStyleElem(elem, styleObj) {
-  for (let [key, value] of Object.entries(styleObj)) {
-    elem.style[key] = value;
-  }
-}
-
-/* Define layout for counterDiv */
-
+/* Counter min and max limits*/
 const COUNTER_MIN = 0;
-const COUNTER_MAX = "bb";
+const COUNTER_MAX = 10;
 
+/* Inizialize the counter */
 let counterPara = createParagraph(COUNTER_MIN);
 
+/* Define +/- buttons with custom attribute for behaviour pattern */
 let decreaseBtn = createBtn("-", "decrease");
 defineStyleElem(decreaseBtn, {height: "30px", width: "50px"});
 
 let increaseBtn = createBtn("+", "increase");
 defineStyleElem(increaseBtn, {height: "30px", width: "50px"});
 
-let containerBtn = document.createElement('div');
-
 let title = createParagraph("Counter");
 defineStyleElem(title, {display: "inline", marginLeft: "30px", marginRight: "30px"});
 
+/* Container for button and title */
+let containerBtn = document.createElement('div');
 containerBtn.append(decreaseBtn, title, increaseBtn);
 defineStyleElem(containerBtn, {marginTop: "30px", marginBottom: "30px"});
 
+/* Paragraph with the message to user */
 let counterMessage = createParagraph();
 defineStyleElem(counterMessage, {display: "none", fontSize: "20px", marginTop: "20px"});
 
+/* Event Listener with event delegation and behaviour pattern in action */
 containerBtn.addEventListener("click", (event) => {
 
   let action = event.target.dataset.action;
-  let counterHTML = getCounterHTML(counterPara);
-  
+  let counterHTML = +getCounterHTML(counterPara);
+
   switch(action) {
     case "decrease":
       counterHTML = decreaseCounter(counterHTML);
@@ -100,14 +46,21 @@ containerBtn.addEventListener("click", (event) => {
       return false;
   }
 
+  /* 
+    Check if the counter is between the limits and set proper counter.
+    If the user reaches the counter limits min or max, it will show a message to user.
+  */
   if (isValidCounter(counterHTML, COUNTER_MIN, COUNTER_MAX)) {
+
     setCounterHTML(counterPara, counterHTML);
     defineStyleElem(counterMessage, {display: "none"});
+
   } else {
+
     counterMessage.innerHTML = defineMessage(counterHTML, COUNTER_MIN, COUNTER_MAX);
     defineStyleElem(counterMessage, {display: ""});
-  }
 
+  }
 });
 
 counterDiv.append(containerBtn, counterPara, counterMessage);
