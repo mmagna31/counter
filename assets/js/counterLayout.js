302fd0dafd96b501ff597b0c3eb5039756dd373e
mmagna31 <marco.magnarelli@gmail.com>
2021-07-09 19:22:05 +0200

    in progress

--- assets/js/counterLayout.js
+++ assets/js/counterLayout.js
@@ -2,24 +2,12 @@ function defineLayout(num) {
 
   /* return a div with the defined layout */
 
-  let counter = +num;
-
-  let counterBox = document.createElement("div");
+  let counter = bigInt(num);
+  // let counter = +num;
       
-  let counterDisplay = document.createElement("div");
-  counterDisplay.className = "counter-display";
-  
   let counterNum = document.createElement("p");
-  counterDisplay.className = "counter-num";
+  counterNum.className = "counter-num";
   counterNum.innerHTML = counter;
-  
-  let counterMsg = document.createElement("p");
-  counterMsg.className = "message";
-  counterMsg.hidden = true;
-  
-  counterDisplay.append(counterNum, counterMsg);
-  let counterBtn = document.createElement("div");
-  counterBtn.className = "counter-btn";
   
   let decreaseBtn = document.createElement("button");
   decreaseBtn.className = "btn";
@@ -36,38 +24,51 @@ function defineLayout(num) {
   resetBtn.setAttribute("data-action", "reset");
   resetBtn.insertAdjacentHTML('afterbegin', '<i class="fas fa-undo"></i>');
 
+  let counterBtn = document.createElement("div");
+  counterBtn.className = "counter-btn";
   counterBtn.append(decreaseBtn, increaseBtn, resetBtn);
 
   /* Event Listener with event delegation and behaviour pattern in action */
   counterBtn.addEventListener("click", (event) => {
 
-    let action = event.target.closest('button').dataset.action;
+    console.log(counter);
+
+    let btn = event.target.closest('button');
+
+    if (!btn) {
+      return false;
+    }
+
+    let action = btn.dataset.action;
 
     switch(action) {
       case "decrease":
+        console.log("decrease", typeof counter);
         --counter;
         break;
       case "increase":
+        console.log("increase", counter);
         ++counter;
         break;
       case "reset":
+        console.log("in reset", counter);
         counter = num;
         break;
     }
 
-    if (isValidCounter(counter)) {
+    console.log(counter);
+    // if (isValidCounter(counter)) {
       counterNum.innerHTML = counter;
-    } 
+    // } 
 
   });
 
-  counterBox.append(counterDisplay, counterBtn);
+  let counterBox = document.createElement("div");
+  counterBox.append(counterNum, counterBtn);
 
   return counterBox;
 
 }
 
-
-
-let counter = 0; // TO DO: verifica counter iniziale stringa
+let counter = 10000000000000000000000000000000000000000000000000000000000000000000000000000; // TO DO: verifica counter iniziale stringa
 document.querySelector("main").append(defineLayout(counter));
